df8bc563d9bb73207a85f90e4b22ebc7536f8881
Aryan Srivastava <aryansrivastava3004@gmail.com>
2026-06-07 11:59:10 +0530

    Update maximum_wealth.py

--- Day-03/maximum_wealth.py
+++ Day-03/maximum_wealth.py
@@ -1,1 +1,13 @@
+class Solution:
+    def maximumWealth(self, accounts):
 
+        max_wealth = 0
+
+        for customer in accounts:
+
+            wealth = sum(customer)
+
+            if wealth > max_wealth:
+                max_wealth = wealth
+
+        return max_wealth
