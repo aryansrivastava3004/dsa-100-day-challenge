1b1151484fdb8427a71c74b5e1ed22599f65313c
Aryan Srivastava <aryansrivastava3004@gmail.com>
2026-06-07 11:59:29 +0530

    Update kids_with_greatest_number_of_candies.py

--- Day-04/kids_with_greatest_number_of_candies.py
+++ Day-04/kids_with_greatest_number_of_candies.py
@@ -1,1 +1,10 @@
+class Solution:
+    def kidsWithCandies(self, candies, extraCandies):
 
+        result = []
+        max_candies = max(candies)
+
+        for candy in candies:
+            result.append(candy + extraCandies >= max_candies)
+
+        return result
