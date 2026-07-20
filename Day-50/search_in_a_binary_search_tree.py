53b4309bc6f6a6f840f8c4bc34d87902dd23de4b
Aryan Srivastava <aryansrivastava3004@gmail.com>
2026-07-20 20:51:51 +0530

    Create search_in_a_binary_search_tree.py

---
+++ Day-50/search_in_a_binary_search_tree.py
@@ -0,0 +1,10 @@
+class Solution:
+    def searchBST(self, root, val):
+
+        if not root or root.val == val:
+            return root
+
+        if val < root.val:
+            return self.searchBST(root.left, val)
+
+        return self.searchBST(root.right, val)
