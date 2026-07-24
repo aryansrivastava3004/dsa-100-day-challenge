7513d87986936ec4871e06cdc2802e9b66e2cdfd
Aryan Srivastava <aryansrivastava3004@gmail.com>
2026-07-24 23:12:19 +0530

    Create average_of_levels_in_binary_tree.py

---
+++ Day-54/average_of_levels_in_binary_tree.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+class Solution:
+    def averageOfLevels(self, root):
+
+        if not root:
+            return []
+
+        result = []
+        queue = deque([root])
+
+        while queue:
+
+            level_sum = 0
+            level_size = len(queue)
+
+            for _ in range(level_size):
+
+                node = queue.popleft()
+                level_sum += node.val
+
+                if node.left:
+                    queue.append(node.left)
+
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(level_sum / level_size)
+
+        return result
