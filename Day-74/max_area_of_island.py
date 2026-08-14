b306f3060361baaf0a41bcf6f93e29079460e3e8
Aryan Srivastava <aryansrivastava3004@gmail.com>
2026-08-14 23:36:52 +0530

    Create max_area_of_island.py

---
+++ Day-74/max_area_of_island.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maxAreaOfIsland(self, grid):
+
+        rows = len(grid)
+        cols = len(grid[0])
+
+        def dfs(r, c):
+
+            if r < 0 or r >= rows or c < 0 or c >= cols:
+                return 0
+
+            if grid[r][c] == 0:
+                return 0
+
+            grid[r][c] = 0
+
+            area = 1
+
+            area += dfs(r + 1, c)
+            area += dfs(r - 1, c)
+            area += dfs(r, c + 1)
+            area += dfs(r, c - 1)
+
+            return area
+
+        maxArea = 0
+
+        for r in range(rows):
+            for c in range(cols):
+
+                if grid[r][c] == 1:
+                    maxArea = max(maxArea, dfs(r, c))
+
+        return maxArea
