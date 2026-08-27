f3b9d88812fc4c1828eedc714ae2fd6cd6608088
Aryan Srivastava <aryansrivastava3004@gmail.com>
2026-08-27 23:03:21 +0530

    Create find_largest_value_in_each_tree_row.py

---
+++ Day-87/find_largest_value_in_each_tree_row.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+class Solution:
+    def largestValues(self, root):
+
+        if not root:
+            return []
+
+        queue = deque([root])
+        result = []
+
+        while queue:
+
+            level_max = float("-inf")
+
+            for _ in range(len(queue)):
+
+                node = queue.popleft()
+
+                level_max = max(level_max, node.val)
+
+                if node.left:
+                    queue.append(node.left)
+
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(level_max)
+
+        return result
