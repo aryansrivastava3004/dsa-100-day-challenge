35483ed7c4eda034a533ef1833681af5353914e1
Aryan Srivastava <aryansrivastava3004@gmail.com>
2026-08-29 23:47:12 +0530

    Create construct_binary_tree_preorder_postorder.py

---
+++ Day-89/construct_binary_tree_preorder_postorder.py
@@ -0,0 +1,26 @@
+class Solution:
+    def constructFromPrePost(self, preorder, postorder):
+
+        if not preorder:
+            return None
+
+        root = TreeNode(preorder[0])
+
+        if len(preorder) == 1:
+            return root
+
+        left_root = preorder[1]
+
+        left_size = postorder.index(left_root) + 1
+
+        root.left = self.constructFromPrePost(
+            preorder[1:left_size + 1],
+            postorder[:left_size]
+        )
+
+        root.right = self.constructFromPrePost(
+            preorder[left_size + 1:],
+            postorder[left_size:-1]
+        )
+
+        return root
