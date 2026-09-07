67cc7a60087fcc09c311f9683e8653341478c98b
Aryan Srivastava <aryansrivastava3004@gmail.com>
2026-09-07 21:21:15 +0530

    Create 538_Convert_BST_to_Greater_Tree.py

---
+++ Day-98/538_Convert_BST_to_Greater_Tree.py
@@ -0,0 +1,19 @@
+class Solution:
+    def convertBST(self, root):
+        total = 0
+
+        def dfs(node):
+            nonlocal total
+
+            if not node:
+                return
+
+            dfs(node.right)
+
+            total += node.val
+            node.val = total
+
+            dfs(node.left)
+
+        dfs(root)
+        return root
